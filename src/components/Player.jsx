c198f689e91d802a643637af4cb486f186ff8353
larabaker96 <lara.baker96@yahoo.com>
2026-08-26 20:29:12 +0200

    Handle state and change text types and button values

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,32 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({initialName, symbol}) {
+    const [ playerName, setPlayerName ] = useState(initialName);
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editableName = <span className="player-name">{playerName}</span>;
+    //let btnCaption = 'Edit';
+
+    if (isEditing) {
+        editableName = <input type="text" required value={playerName} onChange={handleChange}/>;
+        // btnCaption = 'Save';
+    }
+
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {editableName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>          
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>          
         </li>
     );
 }
