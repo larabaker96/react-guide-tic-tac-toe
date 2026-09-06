cff9c6f922a60884921bb4d7f0ddd12f2e577c59
larabaker96 <lara.baker96@yahoo.com>
2026-09-06 15:37:45 +0200

    Reduce state mgmt, disabling buttons on click.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -27,17 +27,22 @@ export default function GameBoard({ onSelectSquare, turns }){
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
-                <ol>
-                    {row.map((playerSymbol, colIndex) => (
-                        <li key={colIndex}>
-                            <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
-                                {playerSymbol}
-                            </button>
-                        </li>
-                    ))}
-                </ol>
-            </li>)}
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button 
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                                    disabled={playerSymbol !== null}
+                                >
+                                    {playerSymbol}
+                                </button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
         </ol>
     );
 }
